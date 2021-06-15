31ef74a98217242190a02aa2665f1489bf7af4ce
Flavio Ribeiro <flavioribeiro.dev@gmail.com>
2021-06-15 09:30:47 -0300

    Update products description

--- data/items.js
+++ data/items.js
@@ -1,567 +1,567 @@
 export const items = [
     {
         "id": 1,
-        "name": "Alho",
+        "name": "Alho UND (45g)",
         "price": 1.89
     },
     {
         "id": 2,
-        "name": "Abóbora Cabotia",
+        "name": "Abóbora Cabotia UND (500g)",
         "price": 3.56
     },
     {
         "id": 3,
-        "name": "Abóbora Leite",
+        "name": "Abóbora Leite UND (500g)",
         "price": 1.46
     },
     {
         "id": 4,
-        "name": "Abóbora Menina",
+        "name": "Abóbora Menina UND (500g)",
         "price": "02.03"
     },
     {
         "id": 5,
-        "name": "Abobrinha",
+        "name": "Abobrinha UND (250g)",
         "price": 1.92
     },
     {
         "id": 6,
-        "name": "Acelga",
+        "name": "Acelga UND (400g)",
         "price": 2.39
     },
     {
         "id": 7,
-        "name": "Batata Doce",
+        "name": "Batata Doce (250g)",
         "price": 1.12
     },
     {
         "id": 8,
-        "name": "Batata Inglesa",
+        "name": "Batata Inglesa UND (250g)",
         "price": 1.22
     },
     {
         "id": 9,
-        "name": "Batata Asterix",
+        "name": "Batata Asterix UND (250g)",
         "price": 2.1
     },
     {
         "id": 10,
-        "name": "Batata Yacon",
+        "name": "Batata Yacon UND (500g)",
         "price": 5.21
     },
     {
         "id": 11,
-        "name": "Berinjela",
+        "name": "Berinjela UND (250g)",
         "price": 1.69
     },
     {
         "id": 12,
-        "name": "Berinjela Japonesa",
+        "name": "Berinjela Japonesa UND (300g)",
         "price": 2.23
     },
     {
         "id": 13,
-        "name": "Beterraba com folha",
+        "name": "Beterraba com folha UND (200g)",
         "price": 1.41
     },
     {
         "id": 14,
-        "name": "Cenoura com folha",
+        "name": "Cenoura com folha UND (200g)",
         "price": 0.96
     },
     {
         "id": 15,
-        "name": "Cebola Roxa",
+        "name": "Cebola Roxa UND (110g)",
         "price": 0.75
     },
     {
         "id": 16,
-        "name": "Cebola Branca",
+        "name": "Cebola Branca UND (150g)",
         "price": 0.61
     },
     {
         "id": 17,
-        "name": "Chuchu",
+        "name": "Chuchu UND (300g)",
         "price": "01.08"
     },
     {
         "id": 18,
-        "name": "Couve Flor",
+        "name": "Couve Flor UND (300g)",
         "price": 3.87
     },
     {
         "id": 19,
-        "name": "Coco Seco",
+        "name": "Coco Seco UND (500g)",
         "price": 9.98
     },
     {
         "id": 20,
-        "name": "Feijão Vagem",
+        "name": "Feijão Vagem UND (1kg)",
         "price": 11.22
     },
     {
         "id": 21,
-        "name": "Fava",
+        "name": "Fava UND (1kg)",
         "price": 31.73
     },
     {
         "id": 22,
-        "name": "Gengibre",
+        "name": "Gengibre UND (110g)",
         "price": 2.71
     },
     {
         "id": 23,
-        "name": "Jiló",
+        "name": "Jiló UND (50g)",
         "price": 0.25
     },
     {
         "id": 24,
-        "name": "Macaxeira",
+        "name": "Macaxeira UND (500g)",
         "price": 2.21
     },
     {
         "id": 25,
-        "name": "Mandioquinha",
+        "name": "Mandioquinha UND (130g)",
         "price": 3.86
     },
     {
         "id": 26,
-        "name": "Maxixe",
+        "name": "Maxixe UND (50g)",
         "price": 0.75
     },
     {
         "id": 27,
-        "name": "Milho",
+        "name": "Milho UND (170g)",
         "price": 0.5
     },
     {
         "id": 28,
-        "name": "Nabo",
+        "name": "Nabo UND (225g)",
         "price": 1.52
     },
     {
         "id": 29,
-        "name": "Pepino",
+        "name": "Pepino UND (300g)",
         "price": 1.17
     },
     {
         "id": 30,
-        "name": "Pepino Japonês",
+        "name": "Pepino Japonês UND (150g)",
         "price": 1.71
     },
     {
         "id": 31,
-        "name": "Pimenta D. Moça",
+        "name": "Pimenta D. Moça UND (20g)",
         "price": 0.21
     },
     {
         "id": 32,
-        "name": "Pimentão Verde",
+        "name": "Pimentão Verde UND (130g)",
         "price": 1.3
     },
     {
         "id": 33,
-        "name": "Pimentão Colorido",
+        "name": "Pimentão Colorido UND (215g)",
         "price": 5.47
     },
     {
         "id": 34,
-        "name": "Pimenta de Cheiro",
+        "name": "Pimenta de Cheiro UND (50g)",
         "price": 0.43
     },
     {
         "id": 35,
-        "name": "Quiabo",
+        "name": "Quiabo UND (85g)",
         "price": 1.13
     },
     {
         "id": 36,
-        "name": "Rabanete",
+        "name": "Rabanete UND (34g)",
         "price": 0.33
     },
     {
         "id": 37,
-        "name": "Repolho Roxo",
+        "name": "Repolho Roxo UND (1,100g)",
         "price": 11.06
     },
     {
         "id": 38,
-        "name": "Repolho Verde",
+        "name": "Repolho Verde UND (1,100g)",
         "price": 7.84
     },
     {
         "id": 39,
-        "name": "Tomate Cajá",
+        "name": "Tomate Cajá UND (90g)",
         "price": 0.67
     },
     {
         "id": 40,
-        "name": "Tomate L. Vida",
+        "name": "Tomate L. Vida UND (150g)",
         "price": 1.11
     },
     {
         "id": 41,
-        "name": "Tomate Cereja",
+        "name": "Tomate Cereja UND (25g)",
         "price": 10.05
     },
     {
         "id": 42,
-        "name": "Alface USA",
+        "name": "Alface USA 1 UND",
         "price": 4.35
     },
     {
         "id": 43,
-        "name": "Alface Manteiga",
+        "name": "Alface Manteiga 1 UND",
         "price": 3.75
     },
     {
         "id": 44,
-        "name": "Alface Crespa",
+        "name": "Alface Crespa 1 UND",
         "price": 2.1
     },
     {
         "id": 45,
-        "name": "Alface Roxa",
+        "name": "Alface Roxa 1 UND",
         "price": 2.55
     },
     {
         "id": 46,
-        "name": "Agrião",
+        "name": "Agrião 1 UND",
         "price": 3.45
     },
     {
         "id": 47,
-        "name": "Alecrim",
+        "name": "Alecrim 1 UND",
         "price": 6.3
     },
     {
         "id": 48,
-        "name": "Alho Poró",
+        "name": "Alho Poró 1 UND",
         "price": 5.55
     },
     {
         "id": 49,
-        "name": "Almeirão",
+        "name": "Almeirão 1 UND",
         "price": 2.99
     },
     {
         "id": 50,
-        "name": "Arruda",
+        "name": "Arruda 1 UND",
         "price": 1.95
     },
     {
         "id": 51,
-        "name": "Brócolis",
+        "name": "Brócolis 1 UND",
         "price": 8.55
     },
     {
         "id": 52,
-        "name": "Brócolis Ramoso",
+        "name": "Brócolis Ramoso 1 UND",
         "price": 8.55
     },
     {
         "id": 53,
-        "name": "Boldo",
+        "name": "Boldo 1 UND",
         "price": 2.85
     },
     {
         "id": 54,
-        "name": "Capim santo",
+        "name": "Capim santo 1 UND",
         "price": 1.95
     },
     {
         "id": 55,
-        "name": "Cebolinha",
+        "name": "Cebolinha 1 UND",
         "price": 2.18
     },
     {
         "id": 56,
-        "name": "Cheiro Verde-",
+        "name": "Cheiro Verde 1 UND",
         "price": 1.49
     },
     {
         "id": 57,
-        "name": "Cebolinha Especial",
+        "name": "Cebolinha Especial 1 UND",
         "price": 2.1
     },
     {
         "id": 58,
-        "name": "Orégano",
+        "name": "Orégano 1 UND",
         "price": "04.05"
     },
     {
         "id": 59,
-        "name": "Chicória",
+        "name": "Chicória 1 UND",
         "price": 2.55
     },
     {
         "id": 60,
-        "name": "Chinguensai",
+        "name": "Chinguensai 1 UND",
         "price": "04.05"
     },
     {
         "id": 61,
-        "name": "Coentro",
+        "name": "Coentro 1 UND",
         "price": 2.18
     },
     {
         "id": 62,
-        "name": "Escarola",
+        "name": "Escarola 1 UND",
         "price": 2.55
     },
     {
         "id": 63,
-        "name": "Espinafre",
+        "name": "Espinafre 1 UND",
         "price": 2.25
     },
     {
         "id": 64,
-        "name": "Erva Doce",
+        "name": "Erva Doce 1 UND",
         "price": 4.49
     },
     {
         "id": 65,
-        "name": "Hortelã",
+        "name": "Hortelã 1 UND",
         "price": 2.1
     },
     {
         "id": 66,
-        "name": "Hortelã Pimenta",
+        "name": "Hortelã Pimenta 1 UND",
         "price": 2.1
     },
     {
         "id": 67,
-        "name": "Jongome",
+        "name": "Jongome 1 UND",
         "price": 1.49
     },
     {
         "id": 68,
-        "name": "Manjericão",
+        "name": "Manjericão 1 UND",
         "price": 1.65
     },
     {
         "id": 69,
-        "name": "Mostarda",
+        "name": "Mostarda 1 UND",
         "price": 2.55
     },
     {
         "id": 70,
-        "name": "Pak Choi",
+        "name": "Pak Choi 1 UND",
         "price": "04.05"
     },
     {
         "id": 71,
-        "name": "Rúcula",
+        "name": "Rúcula 1 UND",
         "price": 3.45
     },
     {
         "id": 72,
-        "name": "Salsa",
+        "name": "Salsa 1 UND",
         "price": 1.95
     },
     {
         "id": 73,
-        "name": "Salsão",
+        "name": "Salsão 1 UND",
         "price": 8.85
     },
     {
         "id": 74,
-        "name": "Salvia",
+        "name": "Salvia 1 UND",
         "price": 6.75
     },
     {
         "id": 75,
-        "name": "Tomilho",
+        "name": "Tomilho 1 UND",
         "price": 5.99
     },
     {
         "id": 76,
-        "name": "Vinagreira",
+        "name": "Vinagreira 1 UND",
         "price": 1.95
     },
     {
         "id": 77,
-        "name": "Vick",
+        "name": "Vick 1 UND",
         "price": 1.5
     },
     {
         "id": 78,
-        "name": "Aspargo do Papai",
+        "name": "Aspargo do Papai 1 UND",
         "price": 7.35
     },
     {
         "id": 79,
-        "name": "Broto de Alfafa",
+        "name": "Broto de Alfafa 1 UND",
         "price": 13.35
     },
     {
         "id": 80,
-        "name": "Cogumelo Paris",
+        "name": "Cogumelo Paris 1 UND",
         "price": 29.7
     },
     {
         "id": 81,
-        "name": "Shitake",
+        "name": "Shitake 1 UND",
         "price": 29.85
     },
     {
         "id": 82,
-        "name": "Shimeje",
+        "name": "Shimeje 1 UND",
         "price": 29.85
     },
     {
         "id": 83,
-        "name": "Shimeje Branco",
+        "name": "Shimeje Branco 1 UND",
         "price": 29.85
     },
     {
         "id": 84,
-        "name": "Tofu",
+        "name": "Tofu 1 UND",
         "price": 26.85
     },
     {
         "id": 85,
-        "name": "Abacate",
+        "name": "Abacate UND (300g)",
         "price": 3.11
     },
     {
         "id": 86,
-        "name": "Abacaxi Pérola",
+        "name": "Abacaxi Pérola UND (1,200g)",
         "price": 8.91
     },
     {
         "id": 87,
-        "name": "Abacaxi Turiaçu",
+        "name": "Abacaxi Turiaçu UND (1,620g)",
         "price": 14.56
     },
     {
         "id": 88,
-        "name": "Acerola",
+        "name": "Acerola UND ( 1 pacote)",
         "price": 4.35
     },
     {
         "id": 89,
-        "name": "Banana",
+        "name": "Banana UND (1,900g)",
         "price": 10.55
     },
     {
         "id": 90,
-        "name": "Caqui Importado",
+        "name": "Caqui Importado UND (113g)",
         "price": 2.36
     },
     {
         "id": 91,
-        "name": "Goiaba",
+        "name": "Goiaba UND (300g)",
         "price": 4.91
     },
     {
         "id": 92,
-        "name": "Laranja",
+        "name": "Laranja UND (130g)",
         "price": 0.58
     },
     {
         "id": 93,
-        "name": "Laranja Lima",
+        "name": "Laranja Lima UND (110g)",
         "price": 1.2
     },
     {
         "id": 94,
-        "name": "Limão",
+        "name": "Limão UND (100g)",
         "price": 0.27
     },
     {
         "id": 95,
-        "name": "Limão Siciliano",
+        "name": "Limão Siciliano UND (200g)",
         "price": 3.29
     },
     {
         "id": 96,
-        "name": "Maçã",
+        "name": "Maçã UND (130g)",
         "price": 1.17
     },
     {
         "id": 97,
-        "name": "Maçã Verde",
+        "name": "Maçã Verde UND (130g)",
         "price": 3.3
     },
     {
         "id": 98,
-        "name": "Mamão Formosa",
+        "name": "Mamão Formosa UND (1,100g)",
         "price": 4.54
     },
     {
         "id": 99,
-        "name": "Mamão Havai",
+        "name": "Mamão Havai UND (750g)",
         "price": 4.73
     },
     {
         "id": 100,
-        "name": "Manga Palmer",
+        "name": "Manga Palmer UND (330g)",
         "price": 1.96
     },
     {
         "id": 101,
-        "name": "Manga Tommy",
+        "name": "Manga Tommy UND (550g)",
         "price": 3.26
     },
     {
         "id": 102,
-        "name": "Melancia",
+        "name": "Melancia 1/4 (1,500g)",
         "price": 1.65
     },
     {
         "id": 103,
-        "name": "Melão Galia",
+        "name": "Melão Galia UND (1,100g)",
         "price": 6.52
     },
     {
         "id": 104,
-        "name": "Melão Espanhol",
+        "name": "Melão Espanhol UND (1,500g)",
         "price": 5.18
     },
     {
         "id": 105,
-        "name": "Morango",
+        "name": "Morango UND (24g)",
         "price": 3.75
     },
     {
         "id": 106,
-        "name": "Pêra Danju",
+        "name": "Pêra Danju UND (300g)",
         "price": 5.18
     },
     {
         "id": 107,
-        "name": "Pitaya",
+        "name": "Pitaya UND (450g)",
         "price": 10.73
     },
     {
         "id": 108,
-        "name": "Tanja Morgot",
+        "name": "Tanja Morgot UND (130g)",
         "price": 1.17
     },
     {
         "id": 109,
-        "name": "Tanja Mexirica",
+        "name": "Tanja Mexirica UND (200g)",
         "price": 0.75
     },
     {
         "id": 110,
-        "name": "Uva Benita",
+        "name": "Uva Benita UND (500g)",
         "price": 6.38
     },
     {
         "id": 111,
-        "name": "Uva Italia",
+        "name": "Uva Italia UND (300g)",
         "price": 3.83
     },
     {
         "id": 112,
-        "name": "Uva Vitória",
+        "name": "Uva Vitória UND (500g)",
         "price": 11.18
     },
     {
         "id": 113,
-        "name": "Uva Thompson",
+        "name": "Uva Thompson UND (500g)",
         "price": 11.93
     }
 ];
